a2e3d4a73cf67a32a138ac151eccf50bfa00cee3
Luca Albrecht <lal126594@iet-gibb.ch>
2022-05-05 12:43:26 +0200

    Added xTimesPrint function

--- index.js
+++ index.js
@@ -10,5 +10,15 @@ function xTimes(passedFunc, times){
         throw("Second argument doesn't seem to be a number.");
     }
 }
+function xTimesPrint(string, times){
+    if(typeof(times) === "number"){
+        for(let i = 0; i < times; i++){
+            console.log(string);
+        }
+    }
+    else{
+        throw("Second argument doesn't seem to be a number.");
+    }
+}
 
 //xTimes(() => console.log("TEST"), 2);
